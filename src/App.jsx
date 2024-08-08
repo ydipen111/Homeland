6c1c09410d5e79ec6226d1a3a6bf23756e2771de
dipen <aggressivebanda74@gmail.com>
2024-08-08 18:25:38 +0545

    commiting

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl text-red-800 font-bold '>Hello app.js
+
+
+
+
+
+
+
+      </h1>
+
+    </>
+  )
+}
+
+export default App
