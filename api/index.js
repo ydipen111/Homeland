15b70c84f2ab3183a2bac9dea6b33a3310b7a3c4
dipen <aggressivebanda74@gmail.com>
2024-08-09 10:28:24 +0545

    second commit

--- api/index.js
+++ api/index.js
@@ -2,6 +2,26 @@ import express from 'express'
 
 const app = express();
 
+
+app.get("/test", (req, res) => {
+
+  const months = 48;
+  const tuitionFee = 4000;
+  const livingCost = 10000;
+
+  return res.json({
+    total: `Total Cost for course completion is:${months * tuitionFee + livingCost * months} `
+  })
+
+
+
+  res.json()
+})
+
+
+
+
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000ddd');
+  console.log('Server is running on port 30000');
 })
